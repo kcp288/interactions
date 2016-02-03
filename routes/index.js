8303269ae783d494dd5237692cdb58970c1b1473
System Administrator <root@OBNY-KPRENTICE.local>
2016-02-03 15:47:05 -0500

    buttons

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html', { title: 'Interactions Research' });
+});
+router.get('/interact', function(req, res, next) {
+  res.render('interactions.html', {});
 });
 
 module.exports = router;
