ec1bd45b60ba4cd17120629e371540edbb51c814
Kira Prentice <kprentice@OBNY-KPRENTICE.local>
2016-02-22 12:45:05 -0500

    updates to html

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('jade').renderFile);
+app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 
 //app.engine('.html', require('jade'));
